d01c48271becfa7be96517caf13b03bd0681ae9a
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-24 08:15:57 +0530

    did the logic for winner and the reset game

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,56 @@
 <script setup>
-  import {ref, computed} from 'vue'
+import { ref, computed } from "vue";
 
-  const player=ref('X');
-  const board=red([
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']  
-  ])
+const player = ref("X");
+const board = red([
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+]);
 
-  const CalculateWinner =(squares)=>{
-    const lines=[
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-
-      }
+const CalculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
-    return null.
   }
+  return null;
+};
+
+const winner = computed(() => CalculateWinner(board.value.flat()));
+const MakeMove = (x, y) => {
+  if (winner.value) return;
+  if (board.value[x][y] !== "") return;
+  board.value[x][y] = player.value;
+  player.value = player.value === "X" ? "O" : "X";
+};
+
+const ResetGame = () => {
+  board.value = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  player.value = "X";
+};
+
 
 </script>
 
 <template>
-<h1>Hello World</h1>
-  
+  <h1>Hello World</h1>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
